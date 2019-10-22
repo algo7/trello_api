e531e2bb04bb7729b24dd345d96fc44b14334ead
Algorithm <alexlo0612@outlook.com>
2019-10-22 23:14:28 +0200

    add middleware

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 
-app.use(BodyParser.urlencoded({
+
+//Global Constant
+const port = process.env.PORT || 3002;
+
+//Initialize the App
+const app = express();
+
+//Compression Module
+app.use(compression())
+
+//bodyParser Middleware
+app.use(bodyParser.urlencoded({
     extended: false,
     limit: '5mb',
 }));
+
+//Start the app
+app.listen(port, () => {
+    console.log(`the app is listening on ${port}`)
+});
